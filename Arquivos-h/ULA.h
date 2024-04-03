2caea0370f0bc6a5fdbbadb263d94cd583624704
Érick <98136250+Rick512P@users.noreply.github.com>
2024-04-03 12:33:06 -0300

    Adição de Funções e Definições
    Adicionadas funções bin_dec, dec_to_bin, AND, OR e realizadas as seguintes definições: ULA_H e BITS 16

--- Arquivos-h/ULA.h
+++ Arquivos-h/ULA.h
@@ -1,1 +1,11 @@
-int ULA(char opc[5], char fc[4]);
+#ifndef ULA_H
+#define ULA_H
+#define BITS 16 
+
+void ULA(char opc[5], char fc[4], char Source[4], char Target[4], char Dest[4]);
+void bin_dec(char Source[], char Target[], char Dest[], int *rs, int *rt, int *rd);
+char* dec_to_bin(int decimal, char binary[]);
+void AND(char Source[], char Target[], char Dest[]);
+void OR(char Source[], char Target[], char Dest[]);
+
+#endif
