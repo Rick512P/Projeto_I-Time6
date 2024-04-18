a7e2ee65042a5c8982da9f8da1c5e91b41240da6
Erick <ricksantosmatos@gmail.com>
2024-04-18 17:02:46 -0300

    tamanho_vetor substituido de forma provisória pela constante 256 para testes

--- Arquivos-c/Salvar_Dados_na_Memoria.c
+++ Arquivos-c/Salvar_Dados_na_Memoria.c
@@ -2,14 +2,14 @@
 
 // Função para salvar os dados de memória em um vetor
 int* salvar_dados_memoria(int *dados_memoria) {
-    int *vetor = (int*)malloc(tamanho_vetor * sizeof(int)); // quem é tamanho_vetor?
+    int *vetor = (int*)malloc(256 * sizeof(int)); // tamanho_vetor substituido de forma provisória pela constante 256 para testes
 
     if (vetor == NULL) {
     
         return NULL;
     }
 
-    for (int i = 0; i < tamanho_vetor; i++) {
+    for (int i = 0; i < 256; i++) {
         vetor[i] = dados_memoria[i]; // Copia os dados de memória para o vetor
     }
 
